589d0f1ef85e94586881ec20006422c03d0a0380
Nicholas Barbosa <nicholas.henrique0604@hotmail.com>
2026-06-08 17:30:10 -0300

    feat: domain model creation

---
+++ src/main/java/com/ebanx/account/model/Account.java
@@ -0,0 +1,23 @@
+package com.ebanx.account.model;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Account {
+
+	private String id;
+	private BigDecimal balance;
+
+}
